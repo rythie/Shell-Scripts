165d11b26d194598af1af66d8a207f03925c36a1
Richard Cunningham <github@richardcunningham.co.uk>
2012-06-10 11:52:58 +0100

    Add a few comments

--- low_memory_warn.sh
+++ low_memory_warn.sh
@@ -1,7 +1,19 @@
+#!/bin/sh
+#
+# Warn when your system gets low in memory
+#
+# Use Ok, to accept warning a get further ones
+# Use Quit, to stop getting messages
+#
+
+#Config
+min_memory=500 #In MB
+
+#Code
 mem=`free -m |grep + |tr -s " " |cut -f4 -d' '`
 while [ true ]
 do
-	if [ $mem -lt 500 ]
+	if [ $mem -lt $min_memory ]
 	then
 		xmessage "Low Memory: " $mem "Mb" -buttons ok:1,quit:99
 		if [ $? == 99 ] #handle quit
