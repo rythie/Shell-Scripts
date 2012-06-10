e124af7d3d9b7180ae7a63470834bef4302d93d6
Richard Cunningham <github@richardcunningham.co.uk>
2012-06-10 16:04:40 +0100

    Use zenity instead of xmessage to get Gnome style dialogs

--- low_memory_warn.sh
+++ low_memory_warn.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Warn when your system gets low on memory
 #
@@ -15,8 +15,8 @@ while [ true ]
 do
 	if [ $mem -lt $min_memory ]
 	then
-		xmessage "Low Memory: " $mem "Mb" -buttons ok:1,quit:99
-		if [ $? == 99 ] #handle quit
+		zenity --question --text "Warning Low Memory, only $mem Mb Remaining" --cancel-label="Quit and Stop Checking" --ok-label="Ok and Keep Checking" --title="Low Memory" 
+		if [ "$?" == "1" ] #handle quit
 		then
 			exit
 		fi
